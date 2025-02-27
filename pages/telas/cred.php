a76bea241ac92735fd6766b736a265ef2fc10661
Volnei Prado <volneifjv@gmail.com>
2025-02-27 16:11:54 -0300

    Pagamento Cartão

---
+++ pages/telas/cred.php
@@ -0,0 +1,207 @@
+<style>
+  body {
+    background-color: #f8f9fa;
+  }
+
+  .card {
+    border-radius: 10px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  }
+
+  .card-header {
+    background-color: #007bff;
+    color: white;
+    font-weight: bold;
+    text-align: center;
+    border-radius: 10px 10px 0 0;
+  }
+
+  .form-group label {
+    font-weight: bold;
+  }
+
+  .btn-primary {
+    width: 100%;
+    font-size: 18px;
+    border-radius: 5px;
+  }
+</style>
+
+<?php
+  include "pages/telas/parcelas.php";
+?>
+
+<div class="container py-5">
+  <div class="row justify-content-center">
+    <div class="col-md-6">
+      <div class="card">
+        <div class="card-header">
+          <h3>Dados do Cartão</h3>
+        </div>
+        <div class="card-body">
+          <form id="paymentForm" method="POST" action="home.php?l=<?=base64_encode(22)?>">
+            <div class="mb-3">
+              <label for="cardHolderName">Nome (Como está no Cartão)</label>
+              <input type="text" class="form-control" id="cardHolderName" name="cardHolderName" required placeholder="Nome">
+            </div>
+            <div class="mb-3">
+              <label for="cpf">CPF do Titular</label>
+              <input type="text" class="form-control" id="cpf" name="cpf" required placeholder="CPF">
+            </div>
+            <div class="mb-3">
+              <label for="payerEmail">E-mail</label>
+              <input type="email" class="form-control" id="payerEmail" name="payerEmail" required placeholder="E-mail">
+            </div>
+            <div class="mb-3">
+              <label for="cardNumber">Número do Cartão</label>
+              <input type="text" class="form-control" id="cardNumber" name="cardNumber" required placeholder="Número do Cartão" onkeydown="getCardBrand(this.value)" onkeyup="getCardBrand(this.value)">
+            </div>
+            <div class="row">
+              <div class="col-md-4">
+                <div class="mb-3">
+                  <label for="expirationMonth">Mês Validade</label>
+                  <select class="form-control" id="expirationMonth" name="expirationMonth" required>
+                    <option value=""></option>
+                    <?php
+                      for($m=1;$m<=12;$m++){
+                        echo "<option value='".str_pad($m , 2 , '0' , STR_PAD_LEFT)."'>".str_pad($m , 2 , '0' , STR_PAD_LEFT)."</option>";
+                      }
+                    ?>
+                  </select>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="mb-3">
+                  <label for="expirationYear">Ano Validade</label>
+                  <select class="form-control" id="expirationYear" name="expirationYear" required>
+                    <option value=""></option>
+                    <?php
+                      $anoAtual = date("Y");
+                      $anoMax = $anoAtual+15;
+                      for($a=$anoAtual;$a<=$anoMax;$a++){
+                        echo "<option value='$a'>$a</option>";
+                      }
+                    ?>
+                  </select>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="mb-3">
+                  <label for="securityCode">CVV</label>
+                  <input type="text" class="form-control" id="securityCode" name="securityCode" required placeholder="CVV">
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-12">
+                <div class="mb-3">
+                  <label for="parcelas">Parcelas</label>
+                  <select class="form-control" id="parcelas" name="parcelas" required>
+                    <?php
+                      $totalParcelas = parcelas($valor);
+                      /*if($totalParcelas > 1){
+                        for($p=2;$p<=$totalParcelas;$p++){
+                          $n_parcelas = ($valor/$p) * (1+(0.01*$p));
+                          $valor_total = $n_parcelas*$p;
+                          echo "<option value='$p'>".$p."x R$ ".number_format($n_parcelas, 2, ',', '.')."  &nbsp;&nbsp;&nbsp; R$ ".number_format($valor_total, 2, ',', '.')."</option>";
+                        }
+                      }*/
+
+                      foreach($parcelas_mp as $prestacoes){
+                        echo "<option value='".$prestacoes["installments"]."'>".$prestacoes["recommended_message"]."</option>";
+                      }
+                    ?>
+                  </select>
+                </div>
+              </div>
+
+            </div>
+
+            <input type="hidden" name="token" id="token">
+            <input type="hidden" name="valor" id="valor" value="<?=$valor?>">
+            <input type="hidden" name="bandeira" id="bandeira">
+            <button type="submit" class="btn btn-primary">Pagar</button>
+
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<script>
+  const mp = new MercadoPago("TEST-09e09a42-40b3-490d-bdbe-e7df1444c890");
+
+  document.getElementById("paymentForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Impede o envio imediato do formulário
+
+    // Coleta e sanitiza os dados
+    const cardData = {
+      cardNumber: document.getElementById("cardNumber").value.replace(/\s/g, ''),
+      cardExpirationMonth: document.getElementById("expirationMonth").value.replace(/\s/g, ''),
+      cardExpirationYear: document.getElementById("expirationYear").value.replace(/\s/g, ''),
+      securityCode: document.getElementById("securityCode").value.replace(/\s/g, ''),
+      cardholderName: document.getElementById("cardHolderName").value.trim(),
+      identificationType: "CPF", // Pode mudar dependendo do país
+      identificationNumber: "12345678909" // Deve ser um CPF válido
+    };
+
+    mp.createCardToken(cardData).then(function(response) {
+      console.log("Token gerado:", response.id);
+      document.getElementById("token").value = response.id;
+      document.getElementById("paymentForm").submit();
+    }).catch(function(error) {
+      console.error("Erro ao gerar token:", error);
+      alert("Erro ao gerar token: " + JSON.stringify(error));
+    });
+  });
+
+  function totalParcelas(parcelas, valor_puro) {
+    var campo_valor = document.getElementById("valor");
+    var campo_valor_ext = document.getElementById("total");
+
+    // Garantir que parcelas e valor_puro sejam números
+    parcelas = Number(parcelas);
+    valor_puro = Number(valor_puro);
+
+    // Aplicação de juros, se houver mais de uma parcela
+    var novo_valor = parcelas === 1 ? valor_puro : valor_puro * (1 + 0.01 * parcelas);
+
+    // Atualizar campo de valor
+    campo_valor.value = novo_valor;
+
+    // Formatar e exibir valor total
+    campo_valor_ext.value = "R$ " + new Intl.NumberFormat('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(novo_valor);
+  }
+
+  function getCardBrand(cardNumber) {
+    // Remove espaços e caracteres não numéricos
+    cardNumber = cardNumber.replace(/\D/g, "");
+    var bandeira = document.getElementById("bandeira");
+
+    const brands = {
+        visa: /^4[0-9]{12}(?:[0-9]{3})?$/,
+        mastercard: /^5[1-5][0-9]{14}$/,
+        amex: /^3[47][0-9]{13}$/,
+        elo: /^(4011(78|79)|431274|438935|457631|457632|504175|627780|636297|636368|5067[0-9]{2}|4576[0-9]{2}|6504[0-9]{2}|6505[0-9]{2}|6506[0-9]{2}|6507[0-9]{2}|6509[0-9]{2}|6516[0-9]{2}|6550[0-9]{2})[0-9]*$/,
+        hipercard: /^(3841[0-9]{12}|606282[0-9]{10}|637095|637568|637599|637609|637612)[0-9]*$/,
+        diners: /^3(?:0[0-5]|[68][0-9])[0-9]{11}$/,
+        discover: /^6(?:011|5[0-9]{2})[0-9]{12}$/
+    };
+
+    for (let brand in brands) {
+        if (brands[brand].test(cardNumber)) {
+            //return brand;
+            bandeira.value = brand;
+            console.log(brand);
+        }
+    }
+
+    return "Bandeira desconhecida";
+  }
+
+
+
+
+
+</script>
---
+++ pages/telas/pay_cred.php
@@ -0,0 +1,118 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $token = $_POST['token']; // O token do cartão gerado pelo front-end
+    $access_token = "TEST-2231983626109454-100916-9b3f0b3430745b1fb823bd06adf5c218-268837322";
+    $cpf = $_POST["cpf"];
+    $bandeira = $_POST["bandeira"];
+    $cpf = str_replace(".", "", $cpf);
+    $cpf = str_replace("-", "", $cpf);
+    $valor = floatval($_POST["valor"]);
+    $parcelas = intval($_POST["parcelas"]);
+    $user_id = $_SESSION["id"];
+    $cardNumber = str_replace(" ", "", $_POST["cardNumber"]);
+    $card_final = $cardNumber[12].$cardNumber[13].$cardNumber[14].$cardNumber[15];
+    //echo $valor;
+
+    // Dados do pagamento
+    $data = [
+        "transaction_amount" => $valor, // Valor da compra
+        "token" => $token, // Token do cartão vindo do front-end
+        "description" => "Renovação de período V-Plan",
+        "installments" => $parcelas, // Número de parcelas
+        "payment_method_id" => "$bandeira", // Método de pagamento (ex: visa, mastercard, etc.)
+        "payer" => [
+            "email" => $_POST["payerEmail"],
+            "first_name" => "Volnei",
+            "last_name" => "Prado",
+            "identification" => [
+                "type" => "CPF",
+                "number" => "$cpf"
+            ]
+        ]
+    ];
+
+    // Exibir os dados enviados para depuração
+    /*echo "<h3>Dados enviados para o Mercado Pago:</h3>";
+    echo "<pre>";
+    print_r($data);
+    echo "</pre>";*/
+
+    // Inicializar cURL para enviar requisição para API do Mercado Pago
+    $ch = curl_init("https://api.mercadopago.com/v1/payments");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        "Content-Type: application/json",
+        "Authorization: Bearer $access_token"
+    ]);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+
+    // Executar requisição e capturar resposta
+    $response = curl_exec($ch);
+    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE); // Captura o código HTTP da resposta
+    curl_close($ch);
+
+    // Decodificar resposta JSON
+    $result = json_decode($response, true);
+
+    // Exibir a resposta completa para depuração
+    /*echo "<h3>Resposta da API do Mercado Pago:</h3>";
+    echo "<pre>";
+    print_r($result);
+    echo "</pre>";*/
+
+    // Verificar status da resposta
+    if ($http_code == 200 || $http_code == 201) {
+        echo "<h3 style='color: green;'>Pagamento aprovado! ID da transação: " . $result["id"] . "</h3>";
+        $id_pay = $result["id"];
+        $nsu = $result["additional_info"]["nsu_processadora"];
+        $total = $result["transaction_details"]["total_paid_amount"];
+
+        $sql = "INSERT INTO `transactions` VALUES(NULL, $valor, $total, $parcelas, '$card_final', $user_id, NULL, NULL, NULL, 'Cartão', NOW(), 2, $nsu, $id_pay)";
+        mysqli_query($conn, $sql);
+        $id_trans = mysqli_insert_id($conn);
+
+        $group_id = $_SESSION["group_id"];
+        $sqlG = "SELECT * FROM groups WHERE id= $group_id";
+        $execG = mysqli_query($conn, $sqlG);
+        $rowG = mysqli_fetch_array($execG);
+        $expiration = new DateTime($rowG["expiration"]);
+        $data_atual = new DateTime(date("Y-m-d"));
+        $days = round($valor / 1.2);
+
+        if ($expiration > $data_atual) {
+          $nova_data = " expiration + INTERVAL $days DAY";
+        } else {
+          $nova_data = "CURRENT_DATE + INTERVAL $days DAY";
+        }
+
+        $sqlUp = "UPDATE groups SET expiration = $nova_data WHERE id=$group_id";
+        mysqli_query($conn, $sqlUp);
+
+        $sqlG2 = "SELECT * FROM groups WHERE id= $group_id";
+        $execG2 = mysqli_query($conn, $sqlG2);
+        $rowG2 = mysqli_fetch_array($execG2);
+
+        $_SESSION["expiration"] = $rowG2["expiration"];
+
+        echo "<script> window.location='home.php?l=".base64_encode(23)."&i=".base64_encode($id_trans)."' </script>";
+
+    } else {
+        echo "<h3 style='color: red;'>Erro no pagamento!</h3>";
+        echo "Código HTTP: $http_code <br>";
+        echo "Mensagem de erro: " . ($result["message"] ?? "Erro desconhecido") . "<br>";
+
+        // Exibir erros adicionais, se houver
+        if (isset($result["cause"])) {
+            echo "<h4>Detalhes do erro:</h4>";
+            foreach ($result["cause"] as $cause) {
+                echo "Causa: " . ($cause["description"] ?? "Sem descrição") . "<br>";
+            }
+        }
+    }
+
+
+} else {
+    echo "<h3 style='color: red;'>Método inválido!</h3>";
+}
+?>
